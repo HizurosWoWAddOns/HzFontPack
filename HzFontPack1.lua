10bafb618faa555c445163fa1403f5f950961f2b
Hizuro <hizuro@gmx.net>
2018-07-23 13:22:50 +0200

    update to use ace3 options

--- HzFontPack1.lua
+++ HzFontPack1.lua
@@ -10,70 +10,133 @@ Known bugs:
 ]]
 
 HzFontPack1DB = {}
-local addon, ns = ...
-local panel = nil
-local locale = GetLocale()
-local SOL, SML = LibStub("LibSimpleOptions-1.0-be_mod"), LibStub("LibSharedMedia-3.0")
+local addon,ns,panel = ...
+--local SOL = LibStub("LibSimpleOptions-1.0-be_mod");
+local SML = LibStub("LibSharedMedia-3.0");
+local AC = LibStub("AceConfig-3.0");
+local ACD = LibStub("AceConfigDialog-3.0");
 local filepath = "Interface\\Addons\\"..addon.."\\fonts\\"
 local fonts = {
-	["7days"]				= "7days.ttf",
-	["Agency FB"]			= "Agency_FB.ttf",
-	["Alcohole"]			= "Alcohole.ttf",
-	["Antartic"]			= "ANTARG__.TTF",
-	["Architext"]			= "architxt.ttf",
-	["AtomicSushi"]			= "AtomicSushi.ttf",
-	["Avocado"]				= "avocado_.ttf",
-	["BTSE + PS2"]			= "btseps2.TTF",
-	["Brush Script MT"]		= "BRUSHSCI.TTF",
-	["Californian"]			= "CALIFR.TTF",
-	["Chiller"]				= "CHILLER.TTF",
-	["Comic Script Shaded"]	= "SF Comic Script Shaded.ttf",
-	["Comic Script"]		= "SF Comic Script.ttf",
-	["CreepyGirl"]			= "CREERG__.TTF",
-	["Dali"]				= "dali____.ttf",
-	["Dark Horse"]			= "Darkh4.ttf",
-	["DragonFly"]			= "dragonfly.ttf",
-	["Dreamerone"]			= "dfdrone.ttf",
-	["Expressway"]			= "Expressway_Free.ttf",
-	["Facelift"]			= "facerg__.ttf",
-	["Fette Fraktur"]		= "ff______.ttf",
-	["Gizmo"]				= "gizmo___.ttf",
-	["Gushing meadow"]		= "sf gushing meadow.ttf",
-	["Headhunter regular"]	= "he______.ttf",
-	["Japan"]				= "japan.ttf",
-	["Klingondagger"]		= "klingondagger.ttf",
-	["Liquidcrystal"]		= "liquidn.ttf",
-	["Loki cola"]			= "lokicola.ttf",
-	["Lucida Console"]		= "lucon.ttf",
-	["Lucida Sans"]			= "lsans.ttf",
-	["Neurochrome"]			= "neurochr.ttf",
-	["New brilliant"]		= "newbrill.ttf",
-	["Old english text"]	= "oldengl.ttf",
-	["Sony Sketch"]			= "Sony_Sketch_EF.ttf",
-	["Telegraphic"]			= "sf telegraphic.ttf",
-	["Typographerfraktur"]	= "typographerfraktur-medium.ttf",
-	["Visitor1"]			= "visitor1.ttf",
-	["Visitor2"]			= "visitor2.ttf",
+	{"7days",				"7days.ttf",					1},
+	{"Agency FB",			"Agency_FB.ttf",				2},
+	{"Alcohole",			"Alcohole.ttf",					3},
+	{"Antartic",			"ANTARG__.TTF",					4},
+	{"Architext",			"architxt.ttf",					5},
+	{"AtomicSushi",			"AtomicSushi.ttf",				6},
+	{"Avocado",				"avocado_.ttf",					7},
+	{"BTSE + PS2",			"btseps2.TTF",					8},
+	{"Brush Script MT",		"BRUSHSCI.TTF",					9},
+	{"Californian",			"CALIFR.TTF",					10},
+	{"Chiller",				"CHILLER.TTF",					11},
+	{"Comic Script Shaded",	"SF Comic Script Shaded.ttf",	12},
+	{"Comic Script",		"SF Comic Script.ttf",			13},
+	{"CreepyGirl",			"CREERG__.TTF",					14},
+	{"Dali",				"dali____.ttf",					15},
+	{"Dark Horse",			"Darkh4.ttf",					16},
+	{"DragonFly",			"dragonfly.ttf",				17},
+	{"Dreamerone",			"dfdrone.ttf",					18},
+	{"Expressway",			"Expressway_Free.ttf",			19},
+	{"Facelift",			"facerg__.ttf",					20},
+	{"Fette Fraktur",		"ff______.ttf",					21},
+	{"Gizmo",				"gizmo___.ttf",					22},
+	{"Gushing meadow",		"sf gushing meadow.ttf",		23},
+	{"Headhunter regular",	"he______.ttf",					24},
+	{"Japan",				"japan.ttf",					25},
+	{"Klingondagger",		"klingondagger.ttf",			26},
+	{"Liquidcrystal",		"liquidn.ttf",					27},
+	{"Loki cola",			"lokicola.ttf",					28},
+	{"Lucida Console",		"lucon.ttf",					29},
+	{"Lucida Sans",			"lsans.ttf",					30},
+	{"Neurochrome",			"neurochr.ttf",					31},
+	{"New brilliant",		"newbrill.ttf",					32},
+	{"Old english text",	"oldengl.ttf",					33},
+	{"Sony Sketch",			"Sony_Sketch_EF.ttf",			34},
+	{"Telegraphic",			"sf telegraphic.ttf",			35},
+	{"Typographerfraktur",	"typographerfraktur-medium.ttf",36},
+	{"Visitor1",			"visitor1.ttf",					37},
+	{"Visitor2",			"visitor2.ttf",					38},
 }
+
+local _print=print;
+local function print(...)
+	local colors,t,c = {"0099ff","00ff00","ff6060","44ffff","ffff00","ff8800","ff44ff","ffffff"},{},1;
+	for i,v in ipairs({addon..":",...}) do
+		if type(v)=="string" and v:match("||c") then
+			tinsert(t,v)
+		else
+			tinsert(t,"|cff"..colors[c]..tostring(v).."|r");
+			c = c<#colors and c+1 or 1;
+		end
+	end
+	_print(unpack(t));
+end
 
 local L = setmetatable({}, {
 	__index = function(t, k)
-		local v=tostring(k)
-		rawset(t, k, v)
-		return v
+		local v=tostring(k);
+		rawset(t, k, v);
+		return v;
 	end
 })
 
-if locale=="deDE" then
-	L["Thank you for choosing this fontpacks.\nThis frame provide the option to disable fonts from this pack."] = "Danke, dass Sie sich für dieses Paket an Zeichensätzen entschieden haben.\nDieses Fenster bietet Ihnen die Möglichkeit Zeichensätze dieses Pakets zu deaktivieren."
-	L["Enable/Disable %s"] = "Aktiviere/Deaktiviere %s"
-	L["Select all"] = "Alle"
-	L["Select none"] = "Keinen"
-	L["Reload UI"] = "UI neuladen"
---	L["-"] = ""
---	L["-"] = ""
---	L["-"] = ""
+--@do-not-package@
+L["Thanks"] = "Thank you for choosing this fontpacks.\nThis frame provide the option to disable fonts from this pack.";
+L["AddOnLoaded"] = "AddOn loaded..."
+L["AddOnLoadedShow"] = "Show 'AddOn loaded...' message on login"
+L["EnableDisable"] = "Enable/Disable %s"
+L["ReloadUI"] = "Reload UI"
+L["Fonts"] = "Fonts"
+--@end-do-not-package@
+--@localization(locale="enUS", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+
+if LOCALE_deDE then
+--@do-not-package@
+L["Thanks"] = "Danke, dass Sie sich für dieses Paket an Zeichensätzen entschieden haben.\nDieses Fenster bietet Ihnen die Möglichkeit Zeichensätze dieses Pakets zu deaktivieren."
+L["AddOnLoaded"] = "AddOn geladen..."
+L["AddOnLoadedShow"] = "Zeige 'AddOn geladen...' Nachricht beim Login"
+L["EnableDisable"] = "Aktiviere/Deaktiviere %s"
+L["ReloadUI"] = "UI neuladen"
+L["Fonts"] = "Zeichensätze"
+--@end-do-not-package@
+--@localization(locale="deDE", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
 end
+
+if LOCALE_esES then
+--@localization(locale="esES", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
+
+if LOCALE_esMX then
+--@localization(locale="esMX", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
+
+if LOCALE_frFR then
+--@localization(locale="frFR", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
+
+if LOCALE_itIT then
+--@localization(locale="itIT", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
+
+if LOCALE_koKR then
+--@localization(locale="koKR", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
+
+if LOCALE_ptBR or LOCALE_ptPT then
+--@localization(locale="ptBR", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
+
+if LOCALE_ruRU then
+--@localization(locale="ruRU", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
+
+if LOCALE_zhCN then
+--@localization(locale="zhCN", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
+
+if LOCALE_zhTW then
+--@localization(locale="zhTW", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
+
 
 -- -------------------------------------------------- --
 -- Function to Sort a table by the keys               --
@@ -97,111 +160,85 @@ local function pairsByKeys(t, f)
 	return iter
 end
 
-local function OptionPanel(self)
-	local panel = self
-	local title, subText = self:MakeTitleTextAndSubText(addon,L["Thank you for choosing this fontpacks.\nThis frame provide the option to disable fonts from this pack."])
-
-	local scroll = CreateFrame("ScrollFrame", panel:GetName().."_Scroll", self, "UIPanelScrollFrameTemplate")
-	local child = CreateFrame("Frame", panel:GetName().."_ScrollChild", scroll)
-	scroll:SetFrameLevel(scroll:GetFrameLevel() + 1)
-	scroll:SetScrollChild(child)
-	child:SetWidth(1) child:SetHeight(1)
-
-	local bg = CreateFrame("Frame", nil, self)
-	bg:SetPoint("TOPLEFT",scroll,"TOPLEFT",-5,3)
-	bg:SetPoint("BOTTOMRIGHT",scroll,"BOTTOMRIGHT",25,-3)
-	bg:SetBackdrop({ bgFile=[[Interface\COMMON\Common-Input-Border-M]], tile=false })
-
-	scroll:ClearAllPoints()
-	scroll:SetPoint("TOPLEFT",panel,"TOPLEFT",10,-80)
-	scroll:SetPoint("BOTTOMRIGHT",panel,"BOTTOMRIGHT",-29,60)
-
-	child.controls = {}
-	for k, v in pairs(SOL.panelMeta) do
-		child[k] = v
+local function getSetOption(info,value)
+	local key=info[#info];
+	if value~=nil then
+		HzFontPack1DB[key]=value;
 	end
-
-	local entry,last,count,row = {},10,0,"odd"
-	for fontName, fontFile in pairsByKeys(fonts) do
-		entry[count] = child:MakeToggle(
-			'name', fontName,
-			'description', string.format(L["Enable/Disable %s"],fontName),
-			'default', true,
-			'getFunc', function() return HzFontPack1DB[fontName] ~= false end,
-			'setFunc', function(value) HzFontPack1DB[fontName] = value end
-		)
-		entry[count]:SetParent(child)
-		entry[count]:SetPoint("TOPLEFT", entry[count-1] or child, "BOTTOMLEFT", count~=0 and 0 or 10, -6)
-		entry[count].example = entry[count]:CreateFontString(nil,"ARTWORK")
-		entry[count].example:SetFont(filepath..fontFile,15)
-		entry[count].example:SetText("a b c A B C 0 1 2 3")
-		entry[count].example:SetJustifyV("CENTER")
-		entry[count].example:SetPoint("TOPLEFT",entry[count],"TOPLEFT",200,-7)
-
-		count = count + 1
-	end
-
-	local footSpacer = CreateFrame("Frame",nil,entry[count-1])
-	footSpacer:SetWidth(1)
-	footSpacer:SetHeight(6)
-	footSpacer:SetPoint("TOPLEFT",entry[count-1], "BOTTOMLEFT",0,-6)
-
-	local btSelAll = self:MakeButton(
-		'name', L["Select all"],
-		'description', '',
-		'func', function()
-			for fontName, Value in pairs(HzFontPack1DB) do
-				HzFontPack1DB[fontName] = true
-			end
-			child:Refresh()
-		end
-	)
-	local btSelNone = self:MakeButton(
-		'name', L["Select none"],
-		'description', '',
-		'func', function()
-			for fontName, Value in pairs(HzFontPack1DB) do
-				HzFontPack1DB[fontName] = false
-			end
-			child:Refresh()
-		end
-	)
-	local btReload = self:MakeButton(
-		'name', L["Reload UI"],
-		'description', '',
-		'func', function() ReloadUI() end
-	)
-
-	btSelAll:SetPoint("TOPLEFT",scroll,"BOTTOMLEFT",0,-10)
-	btSelNone:SetPoint("TOPLEFT",btSelAll,"TOPRIGHT",10,0)
-	btReload:SetPoint("TOPRIGHT",scroll,"BOTTOMRIGHT",0,-10)
-
-	local footer = panel:CreateFontString(nil,"ARTWORK","GameFontHighlightSmall")
-	footer:SetText("|cff00aaffAuthor: Hizuro (Der Mithrilorden|EU)|r")
-	footer:SetJustifyH("CENTER")
-	footer:SetPoint("BOTTOMRIGHT",-10,10)
+	return HzFontPack1DB[key];
 end
-local panel = SOL.AddOptionsPanel(addon, OptionPanel)
-SOL.AddSlashCommand(addon,"/hz","/hzfp","/hzfontpack")
 
+local function enableDisableAllFonts(info)
+	local state = info[#info]=="enable_all";
+	for i=1,#fonts do
+		HzFontPack1DB[fonts[i][1]] = state;
+	end
+end
 
-local f = CreateFrame("frame")
+local options = {
+	type = "group",
+	name = addon,
+	childGroups="tab",
+	args = {
+		header2 = { type="description", order=1, name=L.Thanks },
+		showAddOnLoaded = { type="toggle", order=2, width="double", name=L.AddOnLoadedShow, get=getSetOption, set=getSetOption },
+		reload = { type="execute", order=3, name=RELOADUI, func=function() ReloadUI(); end },
+		fonts_label = { type="header", order=4, name=L["Fonts"] },
+		enable = { type="description", order=5, fontSize="medium", width="half", name=ENABLE..":" },
+		enable_all  = { type="execute", order=6, width="half", name=ALL, func=enableDisableAllFonts },
+		enable_none = { type="execute", order=7, width="half", name=NONE, func=enableDisableAllFonts },
+		fonts = { type="group", order=8, name=L.Fonts, get=getSetOption,set=getSetOption, args={} }
+	}
+}
+
+local texHeight=26/1024;
+local function addFontToOptions(i)
+	options.args.fonts.args["font"..i] = {
+		type="group", order=i, inline=true, name="",
+		args={
+			[fonts[i][1]] = { type="toggle", order=1, name=fonts[i][1], desc=L.EnableDisable:format(fonts[i][1]) },
+			image = {
+				type="description", order=2, name="",
+				image="Interface\\Addons\\"..addon.."\\font_examples",
+				imageCoords={0,1,(fonts[i][3]-1)*texHeight,fonts[i][3]*texHeight},
+				imageWidth=512, imageHeight=26
+			}
+		}
+	}
+end
+
+local f = CreateFrame("frame");
+
 f:SetScript("OnEvent",function(self, event, name)
-	if event=="ADDON_LOADED" then
-		if name ~= addon then return end
+	if event=="ADDON_LOADED" and name==addon then
 
-		print("|cffff4444"..addon.."|r: |cff44ff44Addon loaded...|r")
-
-		for fontName, fontFile in pairsByKeys(fonts) do
-			if type(fontName) == "string" and fontName ~= "" and type(fontFile) == "string" and fontFile ~= "" and HzFontPack1DB[fontName] ~= false then
-				SML:Register("font", fontName, filepath..fontFile, 255)
-			end
+		if HzFontPack1DB==nil then
+			HzFontPack1DB = {};
 		end
 
-		panel:Refresh()
+		if HzFontPack1DB.showAddOnLoaded==nil then
+			HzFontPack1DB.showAddOnLoaded=true;
+		end
 
-		self:UnregisterEvent("ADDON_LOADED")
-	elseif event=="PLAYER_ENTERING_WORLD" then
+		for i=1, #fonts do
+			if HzFontPack1DB[fonts[i][1]]==nil then
+				HzFontPack1DB[fonts[i][1]] = true;
+			end
+			if HzFontPack1DB[fonts[i][1]] then
+				SML:Register("font", fonts[i][1], filepath .. fonts[i][2], 128);
+			end
+			addFontToOptions(i);
+		end
+
+		if HzFontPack1DB.showAddOnLoaded then
+			print(L.AddOnLoaded);
+		end
+
+		AC:RegisterOptionsTable(addon, options);
+		ACD:AddToBlizOptions(addon);
+
+		self:UnregisterEvent("ADDON_LOADED");
+	elseif event=="PLAYER_LOGIN" then
 		--[[
 		> Push MikScrollingBattleText to reinitialize LibSharedMedia support
 		MSBT looking too early at SharedMedia files for
@@ -214,4 +251,5 @@ f:SetScript("OnEvent",function(self, event, name)
 end)
 
 f:RegisterEvent("ADDON_LOADED")
-f:RegisterEvent("PLAYER_ENTERING_WORLD")
+--f:RegisterEvent("PLAYER_LOGIN")
+
